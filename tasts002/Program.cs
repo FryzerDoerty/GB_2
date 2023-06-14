7a1831e41ed8b7af7ac48737b7b3bebf6d38b2f4
Anastasia <didyoumissme.19s@gmail.com>
2023-06-15 01:32:40 +0300

    Добавили tasts002

---
+++ tasts002/Program.cs
@@ -0,0 +1,9 @@
+﻿//  Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет
+Console.WriteLine("Введите трёхзначное число:");
+int numberA = Convert.ToInt32(Console.ReadLine());
+if (numberA/100==0){Console.WriteLine("Третьей цифры нет");}
+else{
+int B = (numberA%10);
+Console.WriteLine(B);
+}
+Console.ReadKey();
