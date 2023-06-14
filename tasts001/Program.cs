32309e009ff1e8b9f5bcf9c38c5b87e09d9b1bff
Anastasia <didyoumissme.19s@gmail.com>
2023-06-15 01:22:51 +0300

    Добавили tasts001

---
+++ tasts001/Program.cs
@@ -0,0 +1,6 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+Console.WriteLine("Введите трёхзначное число:");
+int numberA = Convert.ToInt32(Console.ReadLine());
+int B = (numberA%100)/10;
+Console.WriteLine(B);
+Console.ReadKey();
